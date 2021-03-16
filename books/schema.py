6b29a590940c7a7f8a27d39f4f9e6f6a21e6dee9
SazidAhmed <sazidahmed.official@gmail.com>
2021-03-16 15:40:55 +0600

    Django GraphQL

---
+++ books/schema.py
@@ -0,0 +1,24 @@
+import graphene
+from graphene_django import DjangoObjectType
+from .models import Books
+
+
+class BooksType(DjangoObjectType):
+    class Meta:
+        model = Books
+        fields = ("id", "title", "excerpt")
+
+class Query(graphene.ObjectType):
+
+    all_books = graphene.List(BooksType)
+
+    def resolve_all_books(root, info):
+        # return Books.objects.all()
+        # return Books.objects.filter(title="django")
+        books = Books.objects.all()
+        print(books)
+        return books
+
+schema = graphene.Schema(query=Query)
+
+Django-Graphine
