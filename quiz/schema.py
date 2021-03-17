1380cc59913e4d6f696428e7fe8ca776a5a1167c
SazidAhmed <sazidahmed.official@gmail.com>
2021-03-17 19:52:18 +0600

    GraphQL Queries

---
+++ quiz/schema.py
@@ -0,0 +1,64 @@
+import graphene
+from graphene_django import DjangoObjectType
+from graphene_django import DjangoListField
+from .models import Quizzes, Category, Question, Answer
+
+
+class CategoryType(DjangoObjectType):
+    class Meta:
+        model = Category
+        fields = ("id","name")
+
+class QuizzesType(DjangoObjectType):
+    class Meta:
+        model = Quizzes
+        fields = ("id","title","category")
+
+class QuestionType(DjangoObjectType):
+    class Meta:
+        model = Question
+        fields = ("title","quiz")
+
+class AnswerType(DjangoObjectType):
+    class Meta:
+        model = Answer
+        fields = ("question","answer_text")
+
+class Query(graphene.ObjectType):
+
+    # quiz = graphene.String()
+    # def resolve_quiz(root, info):
+    #     return f"this is the first question"
+    
+
+    # all_quizzes = DjangoListField(QuizzesType)
+    # all_quizzes = graphene.List(QuizzesType)
+    # def resolve_all_quizzes(root, info):
+        # return Quizzes.objects.filter(id=1)
+
+
+    # all_quizzes = graphene.List(QuizzesType)
+    # all_questions = graphene.List(QuestionType)
+    # def resolve_all_quizzes(root, info):
+    #     return Quizzes.objects.all()
+    # def resolve_all_questions(root, info):
+    #     return Question.objects.all()
+
+
+    # all_quizzes = graphene.Field(QuizzesType, id=graphene.Int())
+    # def resolve_all_quizzes(root, info, id):
+    #     return Quizzes.objects.get(pk=id)
+
+
+
+    all_questions = graphene.Field(QuestionType, id=graphene.Int())
+    all_answers = graphene.List(AnswerType, id=graphene.Int())
+
+    def resolve_all_questions(root, info, id):
+        return Question.objects.get(pk=id)
+    def resolve_all_answers(root, info, id):
+        return Answer.objects.filter(question=id)
+
+schema = graphene.Schema(query=Query)
+
+
