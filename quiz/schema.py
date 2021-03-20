de1e5c17c62b5b8c197220f46b8cef47c317ad2b
SazidAhmed <sazidahmed.official@gmail.com>
2021-03-20 18:18:23 +0600

    mutation done

--- quiz/schema.py
+++ quiz/schema.py
@@ -61,18 +61,18 @@ class Query(graphene.ObjectType):
 # Mutation
 
 #Create data
-class CategoryMutation(graphene.Mutation):
+# class CategoryMutation(graphene.Mutation):
 
-    class Arguments:
-        name = graphene.String(required=True)
+#     class Arguments:
+#         name = graphene.String(required=True)
         
-    category = graphene.Field(CategoryType)
+#     category = graphene.Field(CategoryType)
 
-    @classmethod
-    def mutate(cls, root, info, name):
-        category = Category(name=name)
-        category.save()
-        return CategoryMutation(category=category)
+#     @classmethod
+#     def mutate(cls, root, info, name):
+#         category = Category(name=name)
+#         category.save()
+#         return CategoryMutation(category=category)
 
 #Update data
 # class CategoryMutation(graphene.Mutation):
@@ -91,17 +91,17 @@ class CategoryMutation(graphene.Mutation):
 #         return CategoryMutation(category=category)
 
 #Delete data
-# class CategoryMutation(graphene.Mutation):
+class CategoryMutation(graphene.Mutation):
 
-#     class Arguments:
-#         id = graphene.ID()
+    class Arguments:
+        id = graphene.ID()
         
-#     category = graphene.Field(CategoryType)
+    category = graphene.Field(CategoryType)
 
-#     @classmethod
-#     def mutate(cls, root, info, id):
-#         category = Category.objects.get(id=id)
-#         category.delete()
+    @classmethod
+    def mutate(cls, root, info, id):
+        category = Category.objects.get(id=id)
+        category.delete()
 
 class Mutation(graphene.ObjectType):
     update_category = CategoryMutation.Field()
